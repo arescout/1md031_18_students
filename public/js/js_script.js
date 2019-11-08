662aebd09c3395744849d5db445f60113aee8e90
Oskar Areskog <osar3133@student.uu.se>
2019-11-08 14:42:32 +0100

    Somwhere in the js jungle

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -8,7 +8,17 @@ function MenuItem (name, kCal, cG, cL, iV) {
     this.kCalCheck = function () {
         return [this.name + " is " + this.kCal + " kCal."].toString();
     };
+
+    this.onTheMenu = function() {
+        return [
+           this.name + "\n " +
+           this.kCal + " kCal \n " +
+           "Contains: " + "Gluten -" + this.contGluten+ " Lactose -" + this.contLactose + " \n " +
+           "Is Vegan: " + this.isVegan
+        ].toString();
+    };
 }
 
 let hipsterHalloumi = new MenuItem("the Hipster-Halloumi", 3000, true, true, false );
 console.log(hipsterHalloumi.kCalCheck());
+
